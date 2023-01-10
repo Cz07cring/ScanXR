8f773214b7c80627baab81e8284720bf96eddc2e
Cz07cring <8994999@gmail.com>
2023-01-10 11:16:29 +0800

    域传送检测

--- cmd/main.go
+++ cmd/main.go
@@ -6,22 +6,6 @@ var (
 	targets []string
 )
 
-//func workflow(domain []string) {
-//	options := domainscan.Options{
-//		ProviderConfig: config.Worker.Domainscan.ProviderFile,  //加载subfinder配置文件
-//		SubdomainData:  config.Worker.Domainscan.SubdomainData, //加载子域名字典
-//	}
-//	domainscanRunner, err := domainscan.NewRunner(&options)
-//	if err != nil {
-//		gologger.Error().Msgf("domainscan.NewRunner() err, %v", err)
-//		return
-//	}
-//	results := domainscanRunner.Run(domain)
-//	for _, i2 := range results {
-//		fmt.Println(i2.Domain)
-//	}
-//}
-
 func main() {
 	var domain = []string{
 		"projectdiscovery.io",
